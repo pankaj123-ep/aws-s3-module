f032992070b25b1557638712e4bb003e48323d6a
pankaj <pankaj.epcet@gmail.com>
2023-08-22 16:43:38 +0530

    role update

--- Example/provider.tf
+++ Example/provider.tf
@@ -8,6 +8,14 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "us-east-1"
+ assume_role {
+    role_arn = "arn:aws:sts::064648406443:assumed-role/Terraform-teamcity-role/TeamCity-session"
+    session_name = "TeamCity-session"
+  }
+}
+
 
 
 
